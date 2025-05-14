5cb50e0bbb1b89bcedac70aa02323bb27ca147c6
Guilherme-Ryan31 <133902649+Guilherme-Ryan31@users.noreply.github.com>
2025-05-14 08:52:24 -0400

    Create overview_screen.dart

---
+++ lib/screens/overview_screen.dart
@@ -0,0 +1,9 @@
+// lib/screens/overview_screen.dart
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Tela Visão Geral'));
+  }
+}
